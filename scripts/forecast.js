70dfbc9969a72db946171e93e4a11eb65609bf2d
ben.terem <benterem@gmail.com>
2020-12-07 15:59:15 +0200

    --allow-empty

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,3 +1,9 @@
 // code for API 
 
 const key = ;
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apiKey=${key}&q=${city}`
+};
