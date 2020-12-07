2ce183fa456d6d420940fc0a35944f1333d791f3
ben.terem <benterem@gmail.com>
2020-12-07 16:11:56 +0200

    --allow-empty

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,7 +1,20 @@
 // code for API 
 
-const key = 'RQKXts2GYcogSSwxAmz4nKARaGnRgo3K';
+const key = '';
 
+//get weather info
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base+query);
+    const data = await response.json();
+
+    console.log(data);
+};
+
+// get city info
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
@@ -10,9 +23,14 @@ const getCity = async (city) => {
     const response = await fetch(base+query);
     const data = await response.json();
 
-    return data[0];
+    return data[0]['Key'];
 };
 
-getCity('miami')
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+
+
+
+// getCity('miami')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+getWeather('347936');
