c2eb9ffe9f826b393436bc512152090d1ee75bf3
ben.terem <benterem@gmail.com>
2020-12-07 16:01:37 +0200

    --allow-empty

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,11 +1,11 @@
 // code for API 
 
-const key = ;
+const key = '';
 
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
-    const query = `?apiKey=${key}&q=${city}`
+    const query = `?apikey=${key}&q=${city}`
 
     const response = await fetch(base+query);
     const data = await response.json();
