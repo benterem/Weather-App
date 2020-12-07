7acdbc50cafddac6a58fc67e65d30ff54babd87e
ben.terem <benterem@gmail.com>
2020-12-07 16:00:57 +0200

    --allow-empty

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -6,4 +6,11 @@ const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
     const query = `?apiKey=${key}&q=${city}`
+
+    const response = await fetch(base+query);
+    const data = await response.json();
+
+    console.log(data);
 };
+
+getCity('miami');
